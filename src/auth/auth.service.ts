04e7296dcecfd5c0272efb54f6e00d412901b878
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-24 17:26:55 +0700

    feat: filled in user and auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(email: string, pass: string): Promise<Omit<User, 'password'>> {
+    const user = await this.usersService.findOne(email);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+
+    return result;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Nullable } from '../types/utils';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(email: string): Promise<Nullable<User>> {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+}
