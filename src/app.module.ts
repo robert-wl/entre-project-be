eb5733dade02df116867e623f2b5ef26bb929253
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-26 06:53:55 +0700

    feat: moved application controller service modules into /app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './app/auth/auth.module';
+import { UsersModule } from './app/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { Nullable } from '../types/utils';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Nullable } from '../../types/utils';
 import { User } from '@prisma/client';
 
 @Injectable()
