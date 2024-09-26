9ebb3f41077ce4b947a72d7249a3873c2c9e2afd
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-26 14:43:19 +0700

    feat: used dto for response and added validation interceptor

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginRequestDTO } from './dto/login-request.dto';
+import { LoginResponseDTO } from './dto/login-response.dto';
+import { ResponseValidationInterceptor } from '../interceptors/response-validation.interceptor';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() dto: LoginRequestDTO) {
-    return this.authService.login(dto.email, dto.password);
+  @UseInterceptors(new ResponseValidationInterceptor(LoginResponseDTO))
+  async login(@Body() dto: LoginRequestDTO): Promise<LoginResponseDTO> {
+    return await this.authService.login(dto.email, dto.password);
   }
 }
